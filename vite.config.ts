432e5f8b70601748b49314d039cf5bfc24bac071
WokerSilva <marcosilva@ciencias.unam.mx>
2025-08-08 11:23:00 -0600

    feat: Add vite-plugin-static-copy for static file handling in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '',
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
+  base: '/netWorkingSeminario/', // importante si estás en GitHub Pages
 });
